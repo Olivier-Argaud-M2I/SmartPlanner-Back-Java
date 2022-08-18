e4a7ccafa93b9c05b9d573ffc47be88a3f09ab70
Olivier-Argaud-M2I <Olivier.ARGAUD@labom2iformation.fr>
2022-08-18 16:04:59 +0200

    ajout ressource log pour user mise en place classe Encrypt pour futur encryptage des password

--- src/main/java/fr/m2i/smartplanner/resources/UserResources.java
+++ src/main/java/fr/m2i/smartplanner/resources/UserResources.java
@@ -4,10 +4,7 @@ package fr.m2i.smartplanner.resources;
 import fr.m2i.smartplanner.cruds.UserCrud;
 import fr.m2i.smartplanner.models.User;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import java.util.List;
 
@@ -29,6 +26,25 @@ public class UserResources {
     public List<User> getUsers(){
         UserCrud userCrud = new UserCrud();
         return userCrud.getUsers();
+    }
+
+    @POST
+    @Path("/log")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public User login(User user){
+        UserCrud userCrud = new UserCrud();
+        try{
+            User user1 = userCrud.getUserByName(user.getUserName());
+            if(user.getPassword().equals(user1.getPassword())){
+                return user1;
+            }
+            return null;
+        }
+        catch (Exception e){
+            return null;
+        }
+
     }
 
 
